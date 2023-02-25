e8216ca595d71e6f0be9c6abf993823d6a325323
martinbasile13 <martin_basile@hotmail.com>
2023-02-25 10:47:48 -0300

    ...

--- src/component/skills.jsx
+++ src/component/skills.jsx
@@ -7,41 +7,41 @@ export default function Skills() {
             <h1 className="text-2xl m-3">Front End</h1>
             <div className="flex flex-col items-start justify-start">
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/html.svg" alt="" /> Html5 </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/css.svg" alt="" /> CSS </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/tailwind.svg" alt="" /> TailwindCSS </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/html.svg" alt="" /> Html5 </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/css.svg" alt="" /> CSS </kbd>
+
+                </div>
+                <h1 className="m-2 text-lg">Frameworks CSS</h1>
+                <div className="flex flex-wrap">
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/tailwind.svg" alt="" /> TailwindCSS </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/tailwind.svg" alt="" /> Boostrap </kbd>
                 </div>
                 <h1 className="m-2 text-lg">Lenguajes</h1>
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/javascript.svg" alt="" /> JavaScript </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/typescript.svg" alt="" /> TypeScript </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/javascript.svg" alt="" /> JavaScript </kbd>
                 </div>
                 <h1 className="m-2 text-lg">Framework</h1>
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="/../../icons/react.svg" alt="" /> React JS </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="/../../icons/react.svg" alt="" /> React JS </kbd>
                 </div>
             </div>
             <h1 className="text-2xl m-3">Back End</h1>
             <div className="flex flex-col items-start justify-start">
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/git.svg" alt="" /> Git </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/bash.svg" alt="" /> Bash </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/docker.svg" alt="" /> Docker </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/git.svg" alt="" /> Git </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/bash.svg" alt="" /> Shell </kbd>
                 </div>
                 <h1 className="m-2 text-lg">Bases de datos</h1>
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/mongodb.svg" alt="" /> MongoDB </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/mysql.svg" alt="" /> MySql </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/postgresql.svg" alt="" /> PostgreSql </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/mysql.svg" alt="" /> MySql </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/postgresql.svg" alt="" /> PostgreSql </kbd>
                 </div>
                 <h1 className="m-2 text-lg">Lenguajes</h1>
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/python.svg" alt="" /> python </kbd>
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/javascript.svg" alt="" /> JavaScript </kbd>
+                    <kbd className="kbd kbd-sm m-2 px-2"><img src="../../icons/javascript.svg" alt="" /> JavaScript </kbd>
                 </div>
                 <h1 className="m-2 text-lg">Framework</h1>
                 <div className="flex flex-wrap">
-                    <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/django.svg" alt="" /></kbd>
                     <kbd className="kbd kbd-sm mx-2 px-2"><img src="../../icons/nodejs.svg" alt="" /></kbd>
                 </div>
             </div>
